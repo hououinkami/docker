615c82a8085407584a99639dc56efa40518b6202
hououinkami <hououinkami@gmail.com>
2025-02-09 13:55:08 +0800

    测试

---
+++ q2tg/customize.sh
@@ -0,0 +1,10 @@
+cd ~/Docker/q2tg && \
+# 下载src文件夹
+git clone --filter=blob:none --no-checkout https://github.com/clansty/Q2TG.git
+cd Q2TG
+git sparse-checkout init --cone
+git sparse-checkout set src
+git checkout sleepyfox
+rm -rf ../ui && mv -f ui ../
+rm -rf ../main && mv -f main/src ../main && mv -f main/build.ts ../main/build.ts
+cd .. && rm -rf Q2TG
