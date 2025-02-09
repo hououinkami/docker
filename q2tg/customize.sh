469f985348adc8eae23099c55e8dedd33337828e
hououinkami <hououinkami@gmail.com>
2025-02-09 14:17:18 +0800

    Update customize.sh

--- q2tg/customize.sh
+++ q2tg/customize.sh
@@ -3,7 +3,7 @@ cd ~/Docker/q2tg && \
 git clone --filter=blob:none --no-checkout https://github.com/clansty/Q2TG.git
 cd Q2TG
 git sparse-checkout init --cone
-git sparse-checkout set src
+git sparse-checkout set ui main
 git checkout sleepyfox
 rm -rf ../ui && mv -f ui ../
 rm -rf ../main && mv -f main/src ../main && mv -f main/build.ts ../main/build.ts
