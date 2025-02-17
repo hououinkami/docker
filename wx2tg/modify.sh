6803ce7d1dc5eca9065cccce638ed47863e55375
hououinkami <hououinkami@gmail.com>
2025-02-17 15:29:20 +0800

    文本替换

--- wx2tg/modify.sh
+++ wx2tg/modify.sh
@@ -1,31 +1,15 @@
 cp ./utils.ts ./wechat2tg/src/client/utils.ts
 
+source ./localize.sh
+
 cd ./wechat2tg/src/client
-awk '
-    NR == 1 {
-        print "import { translateMessageType } from '\''./utils'\'';"
-    }
-    /emoji\.gif/ {
-        gsub(/emoji\.gif/, "ステッカー.gif");
-    }
-    /收到一条/ {
-        # 在当前行之前插入一行
-        print "const translatedType = translateMessageType(msg.type());";
-        gsub(/收到一条/, "");
-        gsub(/消息，请在手机上查看/, "");
-        gsub(/\$\{msg\.type\(\)\}/, "[${translatedType}]");
-    }
-    { print }
-' WechatClient.ts > temp && mv temp WechatClient.ts
-awk '
-    /temp_file/ {
-        gsub(/temp_file/, "ファイル");
-    }
-    /文件接收中/ {
-        gsub(/文件接收中/, ""); 
-    }
-    /使用文件传输助手接收/ {
-        gsub(/使用文件传输助手接收/, "ファイル転送で受信"); 
-    }
-    { print }
-' TelegramBotClient.ts > temp && mv temp TelegramBotClient.ts
+awk_script='NR == 1 {print "import { translateMessageType } from '\''./utils'\'';"}'
+
+for key in ${(k)localize}; do
+    value=${localize[$key]}
+    awk_script+="/$key/ {gsub(/$key/, \"$value\");} "
+done
+
+awk "$awk_script 1" WechatClient.ts > temp && mv temp WechatClient.ts
+awk "$awk_script 1" TelegramBotClient.ts > temp && mv temp TelegramBotClient.ts
+awk "$awk_script 1" FileHelperClient.ts > temp && mv temp FileHelperClient.ts
