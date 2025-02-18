210e040b45db920999e9cf35a53fe6faf9b7ec69
hououinkami <hououinkami@gmail.com>
2025-02-18 11:26:53 +0800

    更新本地化文本

--- wx2tg/modify.sh
+++ wx2tg/modify.sh
@@ -14,3 +14,5 @@ done
 awk "$awk_script 1" WechatClient.ts > temp && mv temp WechatClient.ts
 awk "$awk_script 1" TelegramBotClient.ts > temp && mv temp TelegramBotClient.ts
 awk "$awk_script 1" FileHelperClient.ts > temp && mv temp FileHelperClient.ts
+cd ../service
+awk "$awk_script 1" TelegramCommandHelper.ts > temp && mv temp TelegramCommandHelper.ts
