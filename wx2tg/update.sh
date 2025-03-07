ea0897123d7b8d78b207f03f579f3927bb5c8ad2
hououinkami <hououinkami@gmail.com>
2025-03-07 14:33:20 +0800

    update

--- wx2tg/update.sh
+++ wx2tg/update.sh
@@ -2,7 +2,7 @@
 
 # 定义函数
 updateContainer() {
-    if [ "$2" = "true" ]; then
+    if [ "$3" = "true" ]; then
         git clone --filter=blob:none --no-checkout https://github.com/finalpi/wechat2tg.git ../wechat2tg
         cd ../wechat2tg
         git sparse-checkout init --cone
@@ -16,8 +16,13 @@ updateContainer() {
         rm -rf ../wechat2tg
     fi
     curl -o docker-compose.yaml https://raw.githubusercontent.com/hououinkami/docker/refs/heads/main/wx2tg-pad.yaml
-    docker compose pull $1
-    docker-compose up -d --no-deps --remove-orphans $1
+    # docker compose pull $1
+    if [ "$2" = "false" ]; then
+        pull="missing"
+    else
+        pull="always"
+    fi
+    docker-compose up -d --no-deps --remove-orphans --pull "$pull" "$1"
     docker image prune --force
 }
 
@@ -103,7 +108,7 @@ case $choice in
         echo "使用自编译的测试版镜像并挂载文件..."
         export IMAGE_NAME=hououinkami/wechat2tg-pad:latest
         export CONTAINER_DIR=/app/src
-        updateContainer "wechat2tg" true
+        updateContainer "wechat2tg" true true
         ;;
     3)
         echo "启用DEBUG模式..."
@@ -115,7 +120,7 @@ case $choice in
         echo "使用finalpi的正式版镜像..."
         export IMAGE_NAME=finalpi/wechat2tg-pad:latest
         export CONTAINER_DIR=/app/src
-        updateContainer "wechat2tg" true
+        updateContainer "wechat2tg" true true
         ;;
     5)
         echo "使用wechotd镜像更新gewechat..."
@@ -124,15 +129,10 @@ case $choice in
         ;;
     6)
         echo "使用自编译镜像更新gewechat..."
-        read -p "是否更新自编译镜像？ [默认: false]: " pull
-        pull=${pull:-false}
+        read -p "是否更新自编译镜像？ [默认: false]: " update
+        update=${update:-false}
         export IMAGE_NAME_GEWE=hououinkami/gewe:alpine
-        if [ "$pull" = "false" ]; then
-            curl -o docker-compose.yaml https://raw.githubusercontent.com/hououinkami/docker/refs/heads/main/wx2tg-pad.yaml
-            docker-compose up -d --no-deps --remove-orphans --pull never gewechat
-        else
-            updateContainer "gewechat"
-        fi
+        updateContainer "gewechat" "$update"
         ;;
     *)
         echo "退出！"
