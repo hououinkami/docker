a3f823cd857e6ca894ed94d3a2c3da1a382e253f
hououinkami <hououinkami@gmail.com>
2025-04-21 15:18:49 +0800

    测试

--- wx2tg/handleMsg.ts
+++ wx2tg/handleMsg.ts
@@ -4,16 +4,18 @@ import { MessageTypeUtils } from './MessageTypeUtils'
 export async function handleMsg(
   msg: { type: () => any; text: () => string }
 ) {
-  if (msg.type() === "chat_histroy") {
+  if (msg.type() === "chat_history") {
     const chatHistory = await getChatHistory(msg);
     return `${chatHistory}`;
-  } else if (msg.type() === "123") {
-    return `${msg.text()}`;
+  } else if (msg.type() === "mini_app") {
+    const miniProgram = await getMiniprogram(msg);
+    return `${miniProgram}`;
   } else {
     return `[${MessageTypeUtils.getTypeName(msg.type() + '')}]`;
   }
 }
 
+// 处理聊天记录
 async function getChatHistory(
   msg: { type: () => any; text: () => string }
 ): Promise<string> {
@@ -27,20 +29,59 @@ async function getChatHistory(
     // 解析recorditem
     const recorditemXml = result.msg.appmsg[0].recorditem;
     const recordData = await parseStringPromise(recorditemXml);
+    const itemCount = recordData.recordinfo.datalist[0].$.count;
     // 获取第一项的日期
-    const firstItemDate = recordData.recordinfo.datalist[0].dataitem[0].sourcetime[0].split(' ')[0];
+    const firstItemDate = recordData.recordinfo.datalist[0].dataitem[0].sourcetime[0].split(' ')[0].replace(/-/g, '/');
+    // 获取最后一项的日期
+    const datalistLength = recordData.recordinfo.datalist[0].dataitem.length;
+    const lastIndex = datalistLength - 1;
+    const lastItemDate = recordData.recordinfo.datalist[0].dataitem[lastIndex].sourcetime[0].split(' ')[0].replace(/-/g, '/');
+    let titleDate = firstItemDate;
+    let multiDays = false;
+    if (firstItemDate !== lastItemDate) {
+      multiDays = true;
+      titleDate = `${firstItemDate}～${lastItemDate}`;
+    }
     // 构建聊天记录
-    let chatHistory = `${title}\n${firstItemDate}\n`;
-    // 遍历所有聊天信息
+    let chatHistory = `${title}\n${titleDate}\n件数: ${itemCount}\n`;
+
     const dataItems = recordData.recordinfo.datalist[0].dataitem;
-    dataItems.forEach((item: { 
-      sourcename: string[], 
-      sourcetime: string[], 
-      datadesc: string[] 
-    }) => {
-      chatHistory += `👤${item.sourcename[0]}(${item.sourcetime[0].split(' ')[1]})\n${item.datadesc[0]}\n`;
-    });
+    // 创建数据类型映射
+    let chatContent;
+    const dataTypeMap = {
+      1: "テクスト",
+      2: "画像",
+      4: "動画",
+      5: "リンク",
+      19: "ミニプログラム"
+    };
+    for (const item of dataItems) {
+      // 获取数据类型
+      const dataType = Number(item.$.datatype);  
+      chatContent = item.datadesc?.[0] ?? "";
+      if (dataType === 1) {
+        chatContent = item.datadesc[0];
+      } else if (dataType === 19) {
+        chatContent = `[${dataTypeMap[dataType as keyof typeof dataTypeMap]}]${item.datatitle[0]}`;
+      } else if (dataType === 5) {
+        chatContent = `<a href="${item.link[0]}">${item.datatitle[0]}</a>`
+      } else {
+        chatContent = `[${dataTypeMap[dataType as keyof typeof dataTypeMap] || `不明`}]`
+      }
+      // 正确解析时间
+      const timestamp = item.sourcetime[0];
+      const { date, time } = await formatTime(timestamp);
+      
+      let chatTime = time;
+      if (multiDays === true) {
+        chatTime = `${date} ${time}`;
+      }
+      
+      chatHistory += `👤${item.sourcename[0]}(${chatTime})\n${chatContent}\n`;
+    }
     // 适配Telegram的HTML模式
+
+    // 标题独立
     const lines = chatHistory.split('\n');
     // 前两行合成一个引用块
     let chatLines = [];
@@ -48,21 +89,47 @@ async function getChatHistory(
     // 处理剩余行
     for (let i = 2; i < lines.length; i++) {
       // 发送者匹配：任意文本(时间)
-      if (/^.+\(\d{2}:\d{2}\)$/.test(lines[i])) {
+      if (/^.+\(\d{2}:\d{2}\)$/.test(lines[i]) || /^.+\(\d{2}\/\d{2} \d{2}:\d{2}\)$/.test(lines[i])) {
         chatLines.push(`${lines[i]}`);
       } else {
         chatLines.push(lines[i]);
       }
     }
-
     const chatText = chatLines.join('\n');
-    const htmlText = `${titleText}\n<blockquote expandable>${chatText}</blockquote>`;
+    // const htmlText = `${titleText}\n<blockquote expandable>${chatText}</blockquote>`;
+
+    // 标题合并
+    const htmlText = `<blockquote expandable>${chatHistory}</blockquote>`;
 
     return htmlText;
     
   } catch (error) {
     console.error('チャット履歴処理エラー:', error);
-    return '[チャット履歴]';
+    return `[${MessageTypeUtils.getTypeName(msg.type() + '')}]`;
+  }
+}
+
+// 处理小程序
+async function getMiniprogram(
+  msg: { type: () => any; text: () => string }
+): Promise<string> {
+  const xmlString = await cleanXmlString(msg.text())
+  try {
+    // 解析主XML
+    const result = await parseStringPromise(xmlString);
+    // 获取标题
+    const miniprogramTitle = result.msg.appmsg[0].title[0];
+    // 获取链接
+    const miniprogramUrl = result.msg.appmsg[0].url[0];
+    
+    // 适配Telegram的HTML模式
+    const htmlText = `<a href="${miniprogramUrl}">${miniprogramTitle}</a>`;
+
+    return htmlText;
+    
+  } catch (error) {
+    console.error('ミニプログラム処理エラー:', error);
+    return `[${MessageTypeUtils.getTypeName(msg.type() + '')}]`;
   }
 }
 
@@ -124,4 +191,24 @@ async function cleanXmlString(input: string) {
   input = input.trim();
   
   return input;
+}
+
+async function formatTime(timestamp: string) {
+  // 拆分日期和时间部分
+  const [datePart, timePart] = timestamp.split(' ');
+
+  // 从日期部分获取月/日
+  const [year, month, day] = datePart ? datePart.split('-') : ['', '', ''];
+  const date = `${month}/${day}`;
+
+  // 从时间部分获取小时:分钟
+  const [hour, minute] = timePart ? timePart.split(':') : ['', ''];
+  const time = `${hour}:${minute}`;
+
+  // 返回包含值的对象
+  return {
+    year,
+    date,
+    time
+  };
 }
