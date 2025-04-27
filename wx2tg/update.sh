f5dbe43230ac99e8bdbb4f2c5910faef6eca4264
hououinkami <hououinkami@gmail.com>
2025-04-27 13:42:46 +0800

    更新

--- wx2tg/update.sh
+++ wx2tg/update.sh
@@ -87,7 +87,7 @@ case $choice in
           -H "Accept: application/vnd.github+json" \
           -H "Authorization: Bearer ${GITHUB_TOKEN}" \
           -H "X-GitHub-Api-Version: 2022-11-28" \
-          "https://api.github.com/repos/hououinkami/docker/actions/workflows/wx2tg-amd.yml/dispatches" \
+          "https://api.github.com/repos/hououinkami/docker/actions/workflows/wx2tg-share.yml/dispatches" \
           -d '{
             "ref": "main"
           }'
